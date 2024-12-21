c23d4b9c2a937f157a7b56c006f993af955b36f3
Anbumani <147915150+nbumani@users.noreply.github.com>
2024-12-21 08:07:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleDetails(element) {
+    const details = element.querySelector('.certification-details');
+    details.style.display = details.style.display === 'block' ? 'none' : 'block';
+}
